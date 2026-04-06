3d0ccd312cf7ccd478dac3263cb6fe429e946de4
shreer13 <161942148+shreer13@users.noreply.github.com>
2026-04-06 23:47:24 +0530

    Create Hillstations.java

---
+++ Assignment-4/Hillstations.java
@@ -0,0 +1,10 @@
+class Hillstations {
+
+    void famousfood() {
+        System.out.println("Common hill station food");
+    }
+
+    void famousfor() {
+        System.out.println("Natural beauty");
+    }
+}
