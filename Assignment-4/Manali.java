941e784037484a0f26dae946cfd5cb85761f94d0
shreer13 <161942148+shreer13@users.noreply.github.com>
2026-04-06 23:47:58 +0530

    Add Manali class with food and fame methods

---
+++ Assignment-4/Manali.java
@@ -0,0 +1,10 @@
+class Manali extends Hillstations {
+
+    void famousfood() {
+        System.out.println("Siddu");
+    }
+
+    void famousfor() {
+        System.out.println("Snow mountains");
+    }
+}
