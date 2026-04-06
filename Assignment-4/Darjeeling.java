3dae6dddc2269a2556c0653b4a3f5e15f8af15af
shreer13 <161942148+shreer13@users.noreply.github.com>
2026-04-06 23:48:54 +0530

    Add Darjeeling class with food and fame methods

---
+++ Assignment-4/Darjeeling.java
@@ -0,0 +1,10 @@
+class Darjeeling extends Hillstations {
+
+    void famousfood() {
+        System.out.println("Momos");
+    }
+
+    void famousfor() {
+        System.out.println("Tea plantations");
+    }
+}
