3dee5c0721dd47a705f0b11ec198e515451655a9
shreer13 <161942148+shreer13@users.noreply.github.com>
2026-04-06 23:48:27 +0530

    Create Ooty.java

---
+++ Assignment-4/Ooty.java
@@ -0,0 +1,10 @@
+class Ooty extends Hillstations {
+
+    void famousfood() {
+        System.out.println("Chocolate");
+    }
+
+    void famousfor() {
+        System.out.println("Tea gardens");
+    }
+}
