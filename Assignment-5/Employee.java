c5ede6cb52acdca806bf7af4f8b714aacec8e02b
shreer13 <161942148+shreer13@users.noreply.github.com>
2026-04-20 18:15:22 +0530

    Create Employee.java

---
+++ Assignment-5/Employee.java
@@ -0,0 +1,16 @@
+package Registration.src.com_emp;
+
+public class Employee {
+    private String name;
+    private String email;
+    private String department;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+}
